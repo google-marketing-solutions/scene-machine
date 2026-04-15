02f88a41a2be5c42505d9242571cb2b86e9baaf2
Victor Paunescu <vsp@google.com>
2026-04-15 10:38:30 +0200

    Add shared components.

---
+++ ui/src/shared/confirm-project-delete-dialog.ts
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+/**
+ * Dialog for confirming a delete project action.
+ */
+@Component({
+  selector: 'app-confirm-project-delete-dialog',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+  template: `
+    <div class="dialog-container">
+      <h2 mat-dialog-title>Delete Project</h2>
+      <mat-dialog-content>
+        Are you sure you want to delete this project?<br />
+        This action cannot be undone.
+      </mat-dialog-content>
+      <mat-dialog-actions align="end">
+        <button mat-button [mat-dialog-close]="false">Cancel</button>
+        <button
+          mat-button
+          color="warn"
+          [mat-dialog-close]="true"
+          cdkFocusInitial
+        >
+          Delete
+        </button>
+      </mat-dialog-actions>
+    </div>
+  `,
+  styles: [
+    `
+      .dialog-container {
+        background: var(--mat-sys-surface-container-highest);
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfirmProjectDeleteDialog {
+  readonly dialogRef = inject(MatDialogRef<ConfirmProjectDeleteDialog>);
+}
