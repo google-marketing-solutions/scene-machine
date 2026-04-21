5a51ab528ac00572057b3eaf51fcb11fc1edefc3
Oleg Rakkolainen <oleg.rakkolainen@gmail.com>
2026-04-21 14:17:40 +0000

    Setup page revamp

---
+++ ui/src/app/setup/confirm-template-dialog/confirm-template-dialog.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+/**
+ * Dialog for confirming a template switch action.
+ */
+@Component({
+  selector: 'app-confirm-template-dialog',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+  templateUrl: './confirm-template-dialog.html',
+  styleUrl: './confirm-template-dialog.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfirmTemplateDialog {
+  readonly dialogRef = inject(MatDialogRef<ConfirmTemplateDialog>);
+}
